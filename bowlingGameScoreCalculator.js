9e75b13a7ab66b24f4ddd25667c39dfb9760abb8
Kamaleshen S <kamaleshen.s@gmail.com>
2023-01-18 12:39:30 +0530

    feat: add functionality to calculate total score

---
+++ bowlingGameScoreCalculator.js
@@ -0,0 +1,26 @@
+
+
+const getBowlingGameScore = (rolls) => {
+    if(!Array.isArray(rolls)){
+        throw new Error('Input is not an array');
+    }
+    return rolls.reduce((score, currentPins, currentIndex) => {
+        if(typeof currentValue === 'string'){
+            throw new Error('Input only number array');
+        }
+        if(currentIndex%2 ===0){
+            if(currentPins ===10){
+                score = score + rolls[currentIndex+1] + rolls[currentIndex+2];
+            } else if( (currentPins + rolls[currentIndex+1]) === 10){
+                score = score + rolls[currentIndex+1] + 10;
+            } else{
+                score = score + rolls[currentIndex] + rolls[currentIndex+1];
+            }
+        }
+
+        return score;
+    }, 0);
+};
+
+module.exports = {getBowlingGameScore};
+console.log(getBowlingGameScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]));
